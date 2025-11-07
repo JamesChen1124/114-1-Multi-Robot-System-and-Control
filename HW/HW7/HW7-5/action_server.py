f081fed864ef9ca7ece4eb31f7e5dc310abf2b5e
JamesChen 陳俊言 <z052340266@gmail.com>
2025-11-07 18:51:17 +0800

    Update action_server.py

--- HW/HW7/HW7-5/action_server.py
+++ HW/HW7/HW7-5/action_server.py
@@ -4,26 +4,20 @@ from rclpy.action import ActionServer
 from rclpy.node import Node
 from fibonacci_interface.action import MyFibonacci
 
-
 class MyFibonacciActionServer(Node):
-
     def __init__(self):
         super().__init__('my_fibonacci_action_server')
-
         self._action_server = ActionServer(
             self,
             MyFibonacci,
             'my_fibonacci',
             self.execute_callback
         )
-
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
 
         feedback_msg = MyFibonacci.Feedback()
         feedback_msg.partial_sequence = [0, 1]
-
-
         for i in range(1, goal_handle.request.order):
             next_num = feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i - 1]
             feedback_msg.partial_sequence.append(next_num)
@@ -37,19 +31,14 @@ class MyFibonacciActionServer(Node):
         result.sequence = feedback_msg.partial_sequence
         self.get_logger().info(f'Result: {result.sequence}')
         return result
-
 def main(args=None):
     rclpy.init(args=args)
     action_server = MyFibonacciActionServer()
-
     try:
         rclpy.spin(action_server)
     except KeyboardInterrupt:
         pass
-
     action_server.destroy()
     rclpy.shutdown()
-
-
 if __name__ == '__main__':
     main()
